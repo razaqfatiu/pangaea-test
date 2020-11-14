1240723df4acf4b1b97c55f1a07b7e73f8824881
Fatiu Isiaka <fatiuisiaka@MacBook-Pro.local>
2020-11-14 20:04:32 +0100

    feat: publish topic now take body content in the request

--- controller.js
+++ controller.js
@@ -1,17 +1,20 @@
 const models = require("./db/models/index");
 
-const utilNewTopic = async (topic) => {
+const utilNewTopic = async (topic, msg) => {
   const [checkTopic] = await models.topics.findOrCreate({ where: { topic } });
   const { topicId } = checkTopic.dataValues;
   const { isNewRecord } = checkTopic._options;
   let newMessage;
   if (isNewRecord) {
-    newMessage = await models.messages.create({
+    newMessage = await models.messages.create(
+      {
         topicId,
-        body: "hello",
-      },{
+        body: msg || "hello",
+      },
+      {
         attributes: ["body"],
-      });
+      }
+    );
   } else {
     newMessage = await models.messages.findOne({
       where: { topicId },
@@ -23,9 +26,10 @@ const utilNewTopic = async (topic) => {
 
 const publishTopic = async (req, res) => {
   try {
+    const { message } = req.body;
     let { topic } = req.params;
     topic = topic.toLowerCase();
-    const { newMessage } = await utilNewTopic(topic);
+    const { newMessage } = await utilNewTopic(topic, message);
     return res.status(200).json({ message: newMessage.body });
   } catch (error) {
     return res.status(500).json({ error: error.message });
